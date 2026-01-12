1e56bee8470ba38c6cd6421372ed6c6dcdd5d89e
Jeet-047 <jeet0912majumder@gmail.com>
2026-01-12 10:30:45 +0530

    stage1 task complete

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project demonstrate  an entire 360° view of an MLOps project',
+    author='Jeet Majumder',
+    license='MIT',
+)
